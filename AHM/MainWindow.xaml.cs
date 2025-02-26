40d19355ee0bd10e0599a05f53410e6b1e8bd4d0
Moenes98 <amunes.f98@gmail.com>
2025-02-26 20:47:09 +0200

    Load IFC, About Added, Properties Added

--- AHM/MainWindow.xaml.cs
+++ AHM/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Windows.Data;
 using Microsoft.Win32;
 using Xbim.Common;
 using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
 using Xbim.IO;
 using Xbim.ModelGeometry.Scene;
 using Xbim.Presentation;
@@ -28,7 +30,7 @@ namespace AHM
 
 		}
 
-
+		#region LoadIFC
 		private void LoadButton_Click(object sender, RoutedEventArgs e)
 		{
 			// Create an OpenFileDialog to select an IFC file
@@ -139,6 +141,140 @@ namespace AHM
 			mf.DeflectionAngle = 0.5; // Angular deflection
 			mf.DeflectionTolerance = mf.OneMilliMetre * 1.0; // Linear deflection
 		}
+
+		#endregion
+		private void AboutButton_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(
+			"This fork was done by:\n" +
+			"- Ahmed\n" +
+			"- Hazem\n" +
+			"- Mostafa\n" +
+			"@ ITI-AEC Intake 45",
+			"About",
+			MessageBoxButton.OK,
+			MessageBoxImage.Information
+			);
+		}
+
+		#region Properties Retrieval
+		public class PropertyItem
+		{
+			public string PropertySetName { get; set; } // Name of the property set
+			public string Name { get; set; }           // Name of the property
+			public string Value { get; set; }          // Value of the property
+			public int IfcLabel { get; set; }          // IFC label of the property (optional)
+		}
+
+		private ObservableCollection<PropertyItem> _properties = new ObservableCollection<PropertyItem>();
+
+		/// <summary>
+		/// Loads properties for the selected IFC entity.
+		/// </summary>
+		/// <param name="entity">The selected IFC entity.</param>
+		public void LoadProperties(IPersistEntity entity)
+		{
+			if (entity == null)
+				return;
+
+			// Clear existing properties
+			_properties.Clear();
+
+			// Fill properties
+			FillPropertyData(entity);
+		}
+
+		/// <summary>
+		/// Extracts properties from the IFC entity.
+		/// </summary>
+		/// <param name="entity">The selected IFC entity.</param>
+		private void FillPropertyData(IPersistEntity entity)
+		{
+			if (entity is IIfcObject ifcObject)
+			{
+				// Extract properties from IfcObject
+				foreach (var relDef in ifcObject.IsDefinedBy)
+				{
+					if (relDef.RelatingPropertyDefinition is IIfcPropertySet pSet)
+						AddPropertySet(pSet);
+				}
+			}
+			else if (entity is IIfcTypeObject ifcTypeObject)
+			{
+				// Extract properties from IfcTypeObject
+				if (ifcTypeObject.HasPropertySets != null)
+				{
+					foreach (var pSet in ifcTypeObject.HasPropertySets.OfType<IIfcPropertySet>())
+						AddPropertySet(pSet);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Adds properties from a property set to the collection.
+		/// </summary>
+		/// <param name="pSet">The property set.</param>
+		private void AddPropertySet(IIfcPropertySet pSet)
+		{
+			if (pSet == null)
+				return;
+
+			// Add single-value properties
+			foreach (var item in pSet.HasProperties.OfType<IIfcPropertySingleValue>())
+				AddProperty(item, pSet.Name);
+
+			// Add complex properties (nested properties)
+			foreach (var item in pSet.HasProperties.OfType<IIfcComplexProperty>())
+			{
+				foreach (var composingProperty in item.HasProperties.OfType<IIfcPropertySingleValue>())
+					AddProperty(composingProperty, $"{pSet.Name} / {item.Name}");
+			}
+
+			// Add enumerated properties
+			foreach (var item in pSet.HasProperties.OfType<IIfcPropertyEnumeratedValue>())
+				AddProperty(item, pSet.Name);
+		}
+
+		/// <summary>
+		/// Adds a single-value property to the collection.
+		/// </summary>
+		/// <param name="item">The property.</param>
+		/// <param name="groupName">The name of the property set or group.</param>
+		private void AddProperty(IIfcPropertySingleValue item, string groupName)
+		{
+			_properties.Add(new PropertyItem
+			{
+				IfcLabel = item.EntityLabel,
+				PropertySetName = groupName,
+				Name = item.Name,
+				Value = item.NominalValue?.ToString() ?? ""
+			});
+		}
+
+		/// <summary>
+		/// Adds an enumerated property to the collection.
+		/// </summary>
+		/// <param name="item">The property.</param>
+		/// <param name="groupName">The name of the property set or group.</param>
+		private void AddProperty(IIfcPropertyEnumeratedValue item, string groupName)
+		{
+			foreach (var nomVal in item.EnumerationValues)
+			{
+				_properties.Add(new PropertyItem
+				{
+					IfcLabel = item.EntityLabel,
+					PropertySetName = groupName,
+					Name = item.Name,
+					Value = nomVal?.ToString() ?? ""
+				});
+			}
+		}
+
+		public void OnEntitySelected(IPersistEntity selectedEntity)
+		{
+			LoadProperties(selectedEntity);
+		}
 	}
+	#endregion
 }
 
